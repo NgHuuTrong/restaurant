482d2c3b7e80cc45e42b8fb57c11c897a05979db
NgHuuTrong <trongnh2003@gmail.com>
2023-07-21 09:16:09 +0700

    Setup React Native and first card of Restaurant

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components/native";
+import { StatusBar, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+  flex: 1;
+`;
+
+export const RestaurantsScreen = () => {
+  return (
+    <>
+      <SafeArea>
+        <SearchContainer>
+          <Searchbar />
+        </SearchContainer>
+        <RestaurantListContainer>
+          <RestaurantInfoCard />
+        </RestaurantListContainer>
+      </SafeArea>
+    </>
+  );
+};
